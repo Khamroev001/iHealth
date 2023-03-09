1e3d9037a9bc9326c38fe55a3f01b095336fd8e6
Khamroev001 <sattorkhamroev@gmail.com>
2023-03-09 09:30:49 +0500

    new changes

--- app/src/main/java/khamroev001/myvita/LanguagePrivacy.kt
+++ app/src/main/java/khamroev001/myvita/LanguagePrivacy.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
@@ -13,11 +15,12 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import khamroev001.myvita.databinding.ActivityLanguagePrivacyBinding
 import khamroev001.myvita.model.User
+import java.util.*
 
 class LanguagePrivacy : AppCompatActivity() {
     lateinit var binding: ActivityLanguagePrivacyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityLanguagePrivacyBinding.inflate(layoutInflater)
+        binding = ActivityLanguagePrivacyBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
@@ -25,30 +28,64 @@ class LanguagePrivacy : AppCompatActivity() {
         var gson = Gson()
         var sh = getSharedPreferences("login", MODE_PRIVATE)
         var edit = sh.edit()
-        var status:Boolean
+        var status: Boolean
         var str = sh.getString("user", "")
-        status=(str=="")
+        status = (str == "")
 
-        ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item).also { adapter ->
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.languages,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             binding.spinner.adapter = adapter
         }
-
-        binding.next.setOnClickListener {
-            if (binding.checkbox.isChecked && status) {
-                    finish()
-                    startActivity(Intent(this,SignUpActivity::class.java))
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    1 -> {
+                        setLocale("en")
+                    }
+                    2 -> {
+                        setLocale("uz")
+                    }
+                }
             }
-            if (binding.checkbox.isChecked && !status){
-                    finish()
-                    startActivity(Intent(this,PIN::class.java))
 
-            }
-            else{
-                Toast.makeText(this,"Check the box",Toast.LENGTH_SHORT).show()
+            override fun onNothingSelected(parent: AdapterView<*>?) {
             }
         }
 
+
+        binding.next.setOnClickListener {
+            if (binding.checkbox.isChecked && status) {
+                finish()
+                startActivity(Intent(this, SignUpActivity::class.java))
+            }
+            if (binding.checkbox.isChecked && !status) {
+                finish()
+                startActivity(Intent(this, PIN::class.java))
+
+            } else {
+                Toast.makeText(this, "Check the box", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
+
+    private fun setLocale(lang: String) {
+        val locale = Locale(lang)
+        Locale.setDefault(locale)
+        resources.configuration.setLocale(locale)
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+        var refresh = Intent(this, LanguagePrivacy::class.java)
+        startActivity(refresh)
+    }
 }
+
+
